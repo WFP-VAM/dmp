83e14d4ea53ea63070f92e99afd45ee6d0234785
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-09 10:18:45 -0500

    feat: navbar

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
 
 import { Home } from 'components';
+import { NavBar } from 'components/NavBar';
 
-const HomePage: NextPage = () => <Home />;
+const HomePage: NextPage = () => (
+  <NavBar>
+    <Home />;
+  </NavBar>
+);
 
 export default HomePage;
--- apps/frontend/pages/profile.tsx
+++ apps/frontend/pages/profile.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from 'next';
 
+import { NavBar } from 'components/NavBar';
 import { Profile } from 'components/pages/Profile';
 
-const ProfilePage: NextPage = () => <Profile />;
+const ProfilePage: NextPage = () => (
+  <NavBar>
+    <Profile />;
+  </NavBar>
+);
 
 export default ProfilePage;
