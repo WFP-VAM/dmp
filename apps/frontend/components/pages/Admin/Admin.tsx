fc1a7c9a44576b9a7f4bad4106016dcae1d4dcde
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-09 21:24:32 -0500

    feat: button override styles

--- apps/frontend/components/pages/Admin/Admin.tsx
+++ apps/frontend/components/pages/Admin/Admin.tsx
@@ -4,7 +4,14 @@ import { Box, Button, Typography } from '@mui/material';
 export const Admin = (): JSX.Element => {
   return (
     <Box display="flex" flexDirection="row-reverse" width="100%" pr={2}>
-      <Button variant="contained" size="large" sx={{ borderRadius: 100 }}>
+      <Button
+        variant="contained"
+        size="large"
+        sx={{
+          borderRadius: 100,
+          boxShadow: 6,
+        }}
+      >
         <Typography variant="h6" padding={1}>
           Add User
         </Typography>
---
+++ apps/frontend/theme/overrides/MuiButtonOverrides.ts
@@ -0,0 +1,7 @@
+import { ComponentsOverrides } from '@mui/material';
+
+export const MuiButtonOverrides: ComponentsOverrides['MuiButton'] = {
+  root: {
+    backgroundColor: '#42A5F5',
+  },
+};
