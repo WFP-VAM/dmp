b60bff70c223eff7ce7028aa05035c42a6e18c41
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-09 21:03:42 -0500

    feat: button stylings

--- apps/frontend/components/pages/Admin/Admin.tsx
+++ apps/frontend/components/pages/Admin/Admin.tsx
@@ -1,9 +1,15 @@
-import { Button } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import { Box, Button, Typography } from '@mui/material';
 
 export const Admin = (): JSX.Element => {
   return (
-    <Button variant="contained" size="large">
-      Add User +
-    </Button>
+    <Box display="flex" flexDirection="row-reverse" width="100%" pr={2}>
+      <Button variant="contained" size="large" sx={{ borderRadius: 100 }}>
+        <Typography variant="h6" padding={1}>
+          Add User
+        </Typography>
+        <AddIcon sx={{ ml: 1 }} />
+      </Button>
+    </Box>
   );
 };
