e0a6580b95dc3875633c4fbf9bf16d60718cb4e3
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-17 15:47:23 -0400

    fix: file locations

--- apps/frontend/pages/validation.tsx
+++ apps/frontend/pages/validation.tsx
@@ -1,89 +1,12 @@
-import {
-  Box,
-  FormControl,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-} from '@mui/material';
-import { ChangeEvent, useState } from 'react';
-import { useIntl } from 'react-intl';
+import { NextPage } from 'next';
 
-const disasters = [
-  { labelId: 'validation_params.flood', value: 'flood' },
-  { labelId: 'validation_params.drought', value: 'drought' },
-];
-const incidents = [
-  { labelId: 'validation_params.incidents.storm', value: 'storm' },
-  { labelId: 'validation_params.incidents.fire', value: 'fire' },
-  { labelId: 'validation_params.incidents.lightning', value: 'lightning' },
-  { labelId: 'validation_params.incidents.epidemics', value: 'epidemics' },
-  {
-    labelId: 'validation_params.incidents.river_collapse',
-    value: 'river_collapse',
-  },
-  { labelId: 'validation_params.incidents.inserts', value: 'inserts' },
-  { labelId: 'validation_params.incidents.traffic', value: 'traffic' },
-  { labelId: 'validation_params.incidents.drowning', value: 'drowning' },
-  { labelId: 'validation_params.incidents.collapse', value: 'collapse' },
-  { labelId: 'validation_params.incidents.weapon', value: 'weapon' },
-  { labelId: 'validation_params.incidents.all', value: 'all' },
-];
-const Validation = () => {
-  const intl = useIntl();
+import { NavBar } from 'components/NavBar';
+import { Validation } from 'components/pages/Validation';
 
-  const [disasterType, setDisasterType] = useState('flood');
-  const handleDisasterChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newDisasterType = e.target.value;
-    setDisasterType(newDisasterType);
-  };
+const ValidationPage: NextPage = () => (
+  <NavBar>
+    <Validation />;
+  </NavBar>
+);
 
-  return (
-    <FormControl>
-      <RadioGroup
-        value={disasterType}
-        onChange={handleDisasterChange}
-        sx={{ display: 'flex', flexDirection: 'row' }}
-      >
-        <Box
-          component="fieldset"
-          sx={{ border: '2px solid grey', borderRadius: 2, margin: 2 }}
-        >
-          <legend>Disaster Type</legend>
-          {disasters.map(type => (
-            <FormControlLabel
-              key={type.labelId}
-              value={type.value}
-              control={<Radio />}
-              label={intl.formatMessage({
-                id: type.labelId,
-              })}
-            />
-          ))}
-        </Box>
-        <Box
-          component="fieldset"
-          width={600}
-          sx={{
-            border: '2px solid grey',
-            borderRadius: 2,
-            margin: 2,
-          }}
-        >
-          <legend>Incident Type</legend>
-          {incidents.map(type => (
-            <FormControlLabel
-              key={type.labelId}
-              value={type.value}
-              control={<Radio />}
-              label={intl.formatMessage({
-                id: type.labelId,
-              })}
-            />
-          ))}
-        </Box>
-      </RadioGroup>
-    </FormControl>
-  );
-};
-
-export default Validation;
+export default ValidationPage;
