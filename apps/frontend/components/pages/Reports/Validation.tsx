a200739efcc4f02e880a37ea5f592eb713ec239e
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-20 10:58:58 -0400

    feat: translation values

--- apps/frontend/components/pages/Reports/Validation.tsx
+++ apps/frontend/components/pages/Reports/Validation.tsx
@@ -6,27 +6,27 @@ import {
   RadioGroup,
 } from '@mui/material';
 import { ChangeEvent, useState } from 'react';
-import { useIntl } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 const disasters = [
   { labelId: 'validation_params.flood', value: 'flood' },
   { labelId: 'validation_params.drought', value: 'drought' },
 ];
 const incidents = [
-  { labelId: 'validation_params.incidents.storm', value: 'storm' },
-  { labelId: 'validation_params.incidents.fire', value: 'fire' },
-  { labelId: 'validation_params.incidents.lightning', value: 'lightning' },
-  { labelId: 'validation_params.incidents.epidemics', value: 'epidemics' },
+  { labelId: 'validation_params.incident.storm', value: 'storm' },
+  { labelId: 'validation_params.incident.fire', value: 'fire' },
+  { labelId: 'validation_params.incident.lightning', value: 'lightning' },
+  { labelId: 'validation_params.incident.epidemics', value: 'epidemics' },
   {
-    labelId: 'validation_params.incidents.river_collapse',
+    labelId: 'validation_params.incident.river_collapse',
     value: 'river_collapse',
   },
-  { labelId: 'validation_params.incidents.inserts', value: 'inserts' },
-  { labelId: 'validation_params.incidents.traffic', value: 'traffic' },
-  { labelId: 'validation_params.incidents.drowning', value: 'drowning' },
-  { labelId: 'validation_params.incidents.collapse', value: 'collapse' },
-  { labelId: 'validation_params.incidents.weapon', value: 'weapon' },
-  { labelId: 'validation_params.incidents.all', value: 'all' },
+  { labelId: 'validation_params.incident.insects', value: 'insects' },
+  { labelId: 'validation_params.incident.traffic', value: 'traffic' },
+  { labelId: 'validation_params.incident.drowning', value: 'drowning' },
+  { labelId: 'validation_params.incident.collapse', value: 'collapse' },
+  { labelId: 'validation_params.incident.weapon', value: 'weapon' },
+  { labelId: 'validation_params.incident.all', value: 'all' },
 ];
 export const ReportValidation = () => {
   const intl = useIntl();
@@ -48,7 +48,12 @@ export const ReportValidation = () => {
           component="fieldset"
           sx={{ border: '2px solid grey', borderRadius: 2, margin: 2 }}
         >
-          <legend>Disaster Type</legend>
+          <legend>
+            <FormattedMessage
+              id="validation_params.disaster_type"
+              defaultMessage="Disaster Type"
+            />
+          </legend>
           {disasters.map(type => (
             <FormControlLabel
               key={type.labelId}
@@ -69,7 +74,12 @@ export const ReportValidation = () => {
             margin: 2,
           }}
         >
-          <legend>Incident Type</legend>
+          <legend>
+            <FormattedMessage
+              id="validation_params.incident.type"
+              defaultMessage="Incident Type"
+            />
+          </legend>
           {incidents.map(type => (
             <FormControlLabel
               key={type.labelId}
