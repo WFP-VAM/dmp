28e3ac8e738e5b047e6bf9719b596dbb8a2aa69b
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-21 10:14:22 -0400

    feat: routing

--- apps/frontend/components/NavBar/NavMenuContent.tsx
+++ apps/frontend/components/NavBar/NavMenuContent.tsx
@@ -5,11 +5,13 @@ import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import {
   Box,
   FormControl,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
+  Menu,
   MenuItem,
   Select,
   SelectChangeEvent,
@@ -17,20 +19,23 @@ import {
 } from '@mui/material';
 import Logo from 'next/image';
 import { useRouter } from 'next/router';
-import { FormattedMessage } from 'react-intl';
+import { useState } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import { useLanguageContext } from 'context';
 import { useIsSignedInUserAdmin } from 'services/api/user/useUser';
 import { env } from 'services/env';
 
 export const NavMenuContent = (): JSX.Element => {
+  const intl = useIntl();
   const router = useRouter();
   const isAdmin = useIsSignedInUserAdmin();
 
-  const navMenuListItemsData = [
-    { text: 'Forms', icon: StarIcon },
-    { text: 'Reports', icon: ContentPasteIcon },
-  ];
+  const handleAdminClick = () => {
+    const baseURL = env('NEXT_PUBLIC_API_BASE_URL');
+    const path = new URL('/admin/login', baseURL);
+    void router.push(path);
+  };
 
   const { language, setLanguage } = useLanguageContext();
   const handleLanguageChange = (e: SelectChangeEvent) => {
@@ -38,10 +43,21 @@ export const NavMenuContent = (): JSX.Element => {
     setLanguage(newLanguage);
   };
 
-  const handleAdminClick = () => {
-    const baseURL = env('NEXT_PUBLIC_API_BASE_URL');
-    const path = new URL('/admin/login', baseURL);
+  const [anchorElFormsMenu, setAnchorElFormsMenu] =
+    useState<null | HTMLElement>(null);
+  const openFormsMenu = Boolean(anchorElFormsMenu);
+  const handleFormsMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorElFormsMenu(event.currentTarget);
+  };
+  const handleFormsMenuClose = () => {
+    setAnchorElFormsMenu(null);
+  };
+  const handleFormsRoutingClick = () => {
+    const baseURL = env('NEXT_PUBLIC_BASE_PATH');
+    console.log(baseURL);
+    const path = new URL('/forms/search', baseURL);
     void router.push(path);
+    setAnchorElFormsMenu(null);
   };
 
   return (
@@ -56,26 +72,55 @@ export const NavMenuContent = (): JSX.Element => {
         />
       </Box>
       <List>
-        {navMenuListItemsData.map(item => (
-          <ListItem key={item.text}>
-            <ListItemButton
-              disableTouchRipple={true}
-              sx={{
-                width: '100%',
-                borderRadius: '4px',
-              }}
-            >
-              <ListItemIcon>
-                <item.icon fontSize="medium" key={item.text} />
-              </ListItemIcon>
-              <ListItemText
-                primary={item.text}
-                primaryTypographyProps={{ fontSize: 16 }}
-              />
+        <ListItem key="forms">
+          <ListItemButton
+            disableTouchRipple={true}
+            sx={{
+              width: '100%',
+              borderRadius: '4px',
+            }}
+          >
+            <ListItemIcon>
+              <StarIcon fontSize="medium" key="forms" />
+            </ListItemIcon>
+            <ListItemText
+              primary={intl.formatMessage({ id: 'navigation.forms.header' })}
+              primaryTypographyProps={{ fontSize: 16 }}
+            />
+            <IconButton onClick={handleFormsMenuClick}>
               <ExpandMoreIcon fontSize="small" sx={{ ml: 2 }} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+            </IconButton>
+            <Menu
+              anchorEl={anchorElFormsMenu}
+              open={openFormsMenu}
+              onClose={handleFormsMenuClose}
+            >
+              <MenuItem onClick={handleFormsRoutingClick}>
+                <FormattedMessage id="navigation.forms.search" />
+              </MenuItem>
+            </Menu>
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="reports">
+          <ListItemButton
+            disableTouchRipple={true}
+            sx={{
+              width: '100%',
+              borderRadius: '4px',
+            }}
+          >
+            <ListItemIcon>
+              <ContentPasteIcon fontSize="medium" key="reports" />
+            </ListItemIcon>
+            <ListItemText
+              primary={intl.formatMessage({ id: 'navigation.reports' })}
+              primaryTypographyProps={{ fontSize: 16 }}
+            />
+            <IconButton>
+              <ExpandMoreIcon fontSize="small" sx={{ ml: 2 }} />
+            </IconButton>
+          </ListItemButton>
+        </ListItem>
         {isAdmin && (
           <ListItem key="admin">
             <ListItemButton onClick={handleAdminClick}>
