aa735758387da48aebdce086a90e25fd9cb3ddac
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-21 12:59:19 -0400

    fix: correct ux

--- apps/frontend/components/NavBar/NavMenuContent.tsx
+++ apps/frontend/components/NavBar/NavMenuContent.tsx
@@ -1,17 +1,14 @@
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import StarIcon from '@mui/icons-material/Star';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import {
   Box,
   FormControl,
-  IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemIcon,
   ListItemText,
-  Menu,
   MenuItem,
   Select,
   SelectChangeEvent,
@@ -19,7 +16,6 @@ import {
 } from '@mui/material';
 import Logo from 'next/image';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 
 import { useLanguageContext } from 'context';
@@ -43,21 +39,10 @@ export const NavMenuContent = (): JSX.Element => {
     setLanguage(newLanguage);
   };
 
-  const [anchorElFormsMenu, setAnchorElFormsMenu] =
-    useState<null | HTMLElement>(null);
-  const openFormsMenu = Boolean(anchorElFormsMenu);
-  const handleFormsMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorElFormsMenu(event.currentTarget);
-  };
-  const handleFormsMenuClose = () => {
-    setAnchorElFormsMenu(null);
-  };
-  const handleFormsRoutingClick = () => {
+  const handleRoutingClick = () => {
     const baseURL = env('NEXT_PUBLIC_BASE_PATH');
-    console.log(baseURL);
     const path = new URL('/forms/search', baseURL);
     void router.push(path);
-    setAnchorElFormsMenu(null);
   };
 
   return (
@@ -74,31 +59,19 @@ export const NavMenuContent = (): JSX.Element => {
       <List>
         <ListItem key="forms">
           <ListItemButton
-            disableTouchRipple={true}
             sx={{
               width: '100%',
               borderRadius: '4px',
             }}
+            onClick={handleRoutingClick}
           >
             <ListItemIcon>
               <StarIcon fontSize="medium" key="forms" />
             </ListItemIcon>
             <ListItemText
-              primary={intl.formatMessage({ id: 'navigation.forms.header' })}
+              primary={intl.formatMessage({ id: 'navigation.forms' })}
               primaryTypographyProps={{ fontSize: 16 }}
             />
-            <IconButton onClick={handleFormsMenuClick}>
-              <ExpandMoreIcon fontSize="small" sx={{ ml: 2 }} />
-            </IconButton>
-            <Menu
-              anchorEl={anchorElFormsMenu}
-              open={openFormsMenu}
-              onClose={handleFormsMenuClose}
-            >
-              <MenuItem onClick={handleFormsRoutingClick}>
-                <FormattedMessage id="navigation.forms.search" />
-              </MenuItem>
-            </Menu>
           </ListItemButton>
         </ListItem>
         <ListItem key="reports">
@@ -116,9 +89,6 @@ export const NavMenuContent = (): JSX.Element => {
               primary={intl.formatMessage({ id: 'navigation.reports' })}
               primaryTypographyProps={{ fontSize: 16 }}
             />
-            <IconButton>
-              <ExpandMoreIcon fontSize="small" sx={{ ml: 2 }} />
-            </IconButton>
           </ListItemButton>
         </ListItem>
         {isAdmin && (
