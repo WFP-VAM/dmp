669f4ae1642929bac0dc0c3e276b541d3a1dfab8
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-22 15:40:25 -0400

    refactor: extract disaster filter

--- apps/frontend/components/pages/Forms/Search.tsx
+++ apps/frontend/components/pages/Forms/Search.tsx
@@ -1,99 +1,11 @@
-import {
-  Box,
-  Button,
-  FormControlLabel,
-  Radio,
-  RadioGroup,
-} from '@mui/material';
-import { DisasterMapping, IncidentMapping } from '@wfp-dmp/interfaces';
+import { Box, Button } from '@mui/material';
+import { DisasterMapping } from '@wfp-dmp/interfaces';
 import { useState } from 'react';
-import { Control, Controller, useForm } from 'react-hook-form';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { useForm } from 'react-hook-form';
+import { FormattedMessage } from 'react-intl';
 
+import { DisasterFilter } from 'components/Filters';
 import { useForms } from 'services/forms/useForms';
-
-interface Props {
-  control: Control<{ DisTyp: string }>;
-}
-
-const DisasterFilter = ({ control }: Props): JSX.Element => {
-  const intl = useIntl();
-
-  const disasters = Object.keys(DisasterMapping).map(disaster => {
-    return {
-      labelId: `validation_params.${disaster}`,
-      value: DisasterMapping[disaster],
-    };
-  });
-  const incidents = Object.keys(IncidentMapping).map(incident => {
-    return {
-      labelId: `validation_params.${incident}`,
-      value: IncidentMapping[incident],
-    };
-  });
-
-  return (
-    <Controller
-      name={'DisTyp'}
-      control={control}
-      render={({ field: { onChange, value } }) => (
-        <RadioGroup
-          value={value}
-          onChange={onChange}
-          sx={{ display: 'flex', flexDirection: 'row' }}
-        >
-          <Box
-            component="fieldset"
-            sx={{ border: '2px solid grey', borderRadius: 2, margin: 2 }}
-          >
-            <legend>
-              <FormattedMessage
-                id="validation_params.disaster_type"
-                defaultMessage="Disaster Type"
-              />
-            </legend>
-            {disasters.map(type => (
-              <FormControlLabel
-                key={type.labelId}
-                value={type.value}
-                control={<Radio />}
-                label={intl.formatMessage({
-                  id: type.labelId,
-                })}
-              />
-            ))}
-          </Box>
-          <Box
-            component="fieldset"
-            width={600}
-            sx={{
-              border: '2px solid grey',
-              borderRadius: 2,
-              margin: 2,
-            }}
-          >
-            <legend>
-              <FormattedMessage
-                id="validation_params.incident_type"
-                defaultMessage="Incident Type"
-              />
-            </legend>
-            {incidents.map(type => (
-              <FormControlLabel
-                key={type.labelId}
-                value={type.value}
-                control={<Radio />}
-                label={intl.formatMessage({
-                  id: type.labelId,
-                })}
-              />
-            ))}
-          </Box>
-        </RadioGroup>
-      )}
-    />
-  );
-};
 
 export const FormSearch = () => {
   const { control, handleSubmit } = useForm({
