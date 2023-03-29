ef38a9d2173fa96366e8adfa460c258dbc693d5f
sifelix <felixv@theodo.com>
2023-03-29 14:54:45 -0400

    feat: store all the filters values in the search state

--- apps/frontend/components/pages/Forms/Search.tsx
+++ apps/frontend/components/pages/Forms/Search.tsx
@@ -1,20 +1,39 @@
 import { Box } from '@mui/material';
 import { DisasterMapping } from '@wfp-dmp/interfaces';
+import dayjs from 'dayjs';
 import { useState } from 'react';
 
-import { SearchFilters } from 'components/Filters/SearchFilters';
+import {
+  SearchFilters,
+  SearchFormData,
+} from 'components/Filters/SearchFilters';
 import { TableDisplay } from 'components/TableDisplay';
 import { useForms } from 'services/api/kobo/useForms';
 
-export const FormSearch = () => {
-  const defaultDisaster = DisasterMapping['flood'];
-  const [disasterType, setDisasterType] = useState(defaultDisaster);
+const defaultSearchFormData: SearchFormData = {
+  disTyp: DisasterMapping['flood'],
+  region: {
+    province: '',
+    district: '',
+    commune: '',
+  },
+  dateRange: {
+    startDate: dayjs(new Date()),
+    endDate: dayjs(new Date()),
+  },
+};
 
-  const { data: formData, isLoading } = useForms(disasterType);
+export const FormSearch = () => {
+  const [searchFormData, setSearchFormData] = useState(defaultSearchFormData);
+
+  const { data: formData, isLoading } = useForms(searchFormData.disTyp);
 
   return (
     <Box display="flex" flexDirection="column">
-      <SearchFilters setDisasterType={setDisasterType} />
+      <SearchFilters
+        initSearchFormData={searchFormData}
+        setSearchFormData={setSearchFormData}
+      />
       <TableDisplay isLoading={isLoading} forms={formData} />
     </Box>
   );
