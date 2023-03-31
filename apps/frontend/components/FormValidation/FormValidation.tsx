48db1d0950370b1ffc91e685e3696dfe2aab1178
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-03-31 02:33:27 -0400

    style: style form elements

--- apps/frontend/components/FormValidation/FormValidation.tsx
+++ apps/frontend/components/FormValidation/FormValidation.tsx
@@ -26,40 +26,68 @@ export const FormValidation = ({
       interviewer: formattedForm.reportName,
       disTyp: formattedForm.disasterType,
       phone: formattedForm.phone,
+      // reportDate: new Date(formattedForm.entryDate),
     },
   });
+  console.log(control._formValues);
 
   return (
     <form>
-      <Box display="flex" flexDirection="column">
-        <Controller
-          name="disTyp"
+      <Box display="flex" flexDirection="column" justifyContent={'center'}>
+        <Box display="flex" flexDirection="row">
+          <Box mr={7}>
+            <Controller
+              name="disTyp"
+              control={control}
+              render={({ field: { value, onChange } }) => (
+                <DisasterFilter value={value} onChange={onChange} />
+              )}
+            />
+          </Box>
+          <Controller
+            name="region"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <RegionFilters value={value} onChange={onChange} />
+            )}
+          />
+        </Box>
+        <Box display="flex" flexDirection="row" sx={{ m: 2, mt: 5 }}>
+          <Box sx={{ mr: 6 }}>
+            <Controller
+              name="interviewer"
+              control={control}
+              render={({ field: { value, onChange } }) => (
+                <TextField
+                  label="Reporter Name"
+                  value={value}
+                  onChange={onChange}
+                  sx={{ minWidth: 260 }}
+                />
+              )}
+            />
+          </Box>
+          <Controller
+            name="phone"
+            control={control}
+            render={({ field: { value, onChange } }) => (
+              <TextField
+                label="Reporter Contact"
+                type="number"
+                value={value}
+                onChange={onChange}
+                sx={{ minWidth: 260 }}
+              />
+            )}
+          />
+        </Box>
+        {/* <Controller
+          name="reportDate"
           control={control}
           render={({ field: { value, onChange } }) => (
-            <DisasterFilter value={value} onChange={onChange} />
+            <DatePicker label="Report Date" value={value} onChange={onChange} />
           )}
-        />
-        <Controller
-          name="region"
-          control={control}
-          render={({ field: { value, onChange } }) => (
-            <RegionFilters value={value} onChange={onChange} />
-          )}
-        />
-        <Controller
-          name="interviewer"
-          control={control}
-          render={({ field: { value, onChange } }) => (
-            <TextField value={value} onChange={onChange} />
-          )}
-        />
-        <Controller
-          name="phone"
-          control={control}
-          render={({ field: { value, onChange } }) => (
-            <TextField type="number" value={value} onChange={onChange} />
-          )}
-        />
+        /> */}
       </Box>
     </form>
   );
