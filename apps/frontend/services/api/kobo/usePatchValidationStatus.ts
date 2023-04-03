21be04893990f50f7e108b9535727fce79f83ea9
sifelix <felixv@theodo.com>
2023-04-03 16:08:58 -0400

    feat(frontend): approve reject a form from the detail view

---
+++ apps/frontend/services/api/kobo/usePatchValidationStatus.ts
@@ -0,0 +1,35 @@
+import {
+  DisasterType,
+  ValidationStatusDto,
+  ValidationStatusValue,
+} from '@wfp-dmp/interfaces';
+import path from 'path';
+import useSWRMutation from 'swr/mutation';
+
+import { ApiRoutes } from 'services/api/apiRoutes';
+import { apiClient } from 'services/api/client';
+
+export const usePatchValidationStatus = (
+  formDisasterType: DisasterType,
+  formId: string,
+) => {
+  const { trigger, isMutating } = useSWRMutation(
+    [ApiRoutes.form, formDisasterType, formId],
+    async (
+      [relativePath, disasterType, id],
+      { arg }: { arg: ValidationStatusValue },
+    ) => {
+      const url = path.join(relativePath, 'validationStatus');
+      const { data: updatedValidationStatus } =
+        await apiClient.patch<ValidationStatusDto>(url, {
+          disasterType,
+          id,
+          validationStatusValue: arg,
+        });
+
+      return updatedValidationStatus;
+    },
+  );
+
+  return { trigger, isMutating };
+};
