54fb9cc87eb754a78f4b86c5d74cee3c1af578ea
Peter Speak <peterspeaking@gmail.com>
2023-04-05 08:39:56 -0500

    feat: adding reports tab

---
+++ apps/frontend/pages/report.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+import { LayoutWithNav } from 'components/Layout';
+
+const SearchFormsPage: NextPage = () => (
+  <LayoutWithNav>
+    <div />
+  </LayoutWithNav>
+);
+
+export default SearchFormsPage;
