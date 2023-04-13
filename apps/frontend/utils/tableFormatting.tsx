69e44cd7600c0b652fd7b5944dd94c2fdcb7a42a
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-04-13 07:50:55 -0400

    feat: dropdown type column

--- apps/frontend/utils/tableFormatting.tsx
+++ apps/frontend/utils/tableFormatting.tsx
@@ -6,14 +6,23 @@ export const getColumnSetup = (
   field: string,
   disaster: DisasterType,
   width = 90,
-): GridColDef => ({
-  field,
-  width,
-  editable: true,
-  renderHeader: (params: GridColumnHeaderParams) => (
-    <FormattedMessage id={`table.${disaster}.column.${params.field}`} />
-  ),
-});
+  opts?: {
+    type: 'singleSelect';
+    valueOptions: { value: string; label: string }[];
+  },
+): GridColDef => {
+  const fields = {
+    field,
+    width,
+    editable: true,
+
+    renderHeader: (params: GridColumnHeaderParams) => (
+      <FormattedMessage id={`table.${disaster}.column.${params.field}`} />
+    ),
+  };
+
+  return { ...fields, ...opts };
+};
 
 export const getGroupSetup = (groupId: string, disaster: DisasterType) => ({
   groupId: groupId,
