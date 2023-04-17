88133334a02af68b14d879da101ac45baf9cc8da
p-chavoshi <120116168+p-chavoshi@users.noreply.github.com>
2023-04-17 17:43:29 -0400

    feat: poc for pdf printing

--- apps/frontend/components/pages/Home/Home.tsx
+++ apps/frontend/components/pages/Home/Home.tsx
@@ -1,17 +1,34 @@
 import { Box, Typography } from '@mui/material';
 import { DisasterDtoType } from '@wfp-dmp/interfaces';
+import { forwardRef, useRef } from 'react';
 import { FormattedMessage } from 'react-intl';
+import ReactToPrint from 'react-to-print';
 import useSWR from 'swr';
 
 import { TableDisplay } from 'components/TableDisplay';
 import { ApiRoutes } from 'services/api/apiRoutes';
 import { useGetMe } from 'services/api/user/useUser';
 
+// eslint-disable-next-line react/display-name
+const PrintMe = forwardRef(
+  (
+    props: { isLoading: boolean; forms: DisasterDtoType[] | undefined },
+    ref,
+  ) => {
+    return (
+      <Box ref={ref}>
+        <TableDisplay isLoading={props.isLoading} forms={props.forms} />
+      </Box>
+    );
+  },
+);
+
 export const Home = (): JSX.Element => {
   const user = useGetMe();
   const { data: lastForms, isLoading } = useSWR<DisasterDtoType[]>(
     ApiRoutes.lastForms,
   );
+  const componentRef = useRef(null);
 
   return (
     <Box>
@@ -22,7 +39,12 @@ export const Home = (): JSX.Element => {
           </Typography>
         </>
       )}
-      <TableDisplay isLoading={isLoading} forms={lastForms} />
+      <ReactToPrint
+        trigger={() => <button>Print this out!</button>}
+        content={() => componentRef.current}
+      />
+
+      <PrintMe ref={componentRef} isLoading={isLoading} forms={lastForms} />
     </Box>
   );
 };
