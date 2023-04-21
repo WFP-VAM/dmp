3e86f1fdf917ab50fb9a17d282267b774a951253
sifelix <felixv@theodo.com>
2023-04-21 17:23:40 -0400

    refactor(frontend): move keys in generate detailed report flood

--- apps/frontend/utils/aggregate/flood/generateFloodDetailedReport.ts
+++ apps/frontend/utils/aggregate/flood/generateFloodDetailedReport.ts
@@ -1,13 +1,22 @@
 import { FloodSpecific, KoboCommonKeys } from '@wfp-dmp/interfaces';
+import { omit } from 'lodash';
 
 import { aggregate } from '../aggregate';
 import { filterNFlood } from '../filterNFlood';
-import {
-  countCategoriesKeys,
-  countMultipleChoicesKeys,
-  firstKeys,
-  sumKeys,
-} from './floodDetailedAggregateKeys';
+
+const firstKeys = [KoboCommonKeys.province, KoboCommonKeys.district];
+
+const sumKeys = Object.values(
+  omit(FloodSpecific, [
+    FloodSpecific.floodN,
+    FloodSpecific.RicePrice,
+    FloodSpecific.threat,
+    FloodSpecific.other,
+  ]),
+);
+
+const countCategoriesKeys = [FloodSpecific.RicePrice];
+const countMultipleChoicesKeys = [FloodSpecific.threat];
 
 export const generateFloodDetailedReport = (
   data: Record<string, string | undefined>[],
