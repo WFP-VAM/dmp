44aa34681595888f1fcb2618744536f4d0634466
sifelix <44159363+sifelix@users.noreply.github.com>
2023-04-28 10:56:14 -0400

    Feat/incident tables (#81)

--- apps/frontend/components/Report/IncidentReport/DetailedIncidentReport.tsx
+++ apps/frontend/components/Report/IncidentReport/DetailedIncidentReport.tsx
@@ -1,7 +1,21 @@
+import { DetailedReportDisasterTable } from 'components/DisasterTable/DetailedReportDisasterTable';
+import { incidenTablesMapping } from 'components/FormValidation/IncidentFormValidation/incidentTablesMapping';
+
 export const DetailedIncidentReport = ({
   report,
 }: {
   report: Record<string, string | number | undefined>[];
 }) => {
-  return <div>{JSON.stringify(report)}</div>;
+  return (
+    <>
+      {incidenTablesMapping.map((tableSetting, index) => (
+        <DetailedReportDisasterTable
+          columns={tableSetting.columns}
+          columnGroup={tableSetting.columnGroup}
+          data={report}
+          key={index}
+        />
+      ))}
+    </>
+  );
 };
