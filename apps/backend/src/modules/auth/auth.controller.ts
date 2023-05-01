c69e7ce33ea29d02e5e694fcb0e5989bf9164738
sifelix <44159363+sifelix@users.noreply.github.com>
2023-05-01 14:04:43 -0400

    fix(backend): fix refresh token for local dev (#86)

--- apps/backend/src/modules/auth/auth.controller.ts
+++ apps/backend/src/modules/auth/auth.controller.ts
@@ -20,7 +20,7 @@ export class AuthController {
     res.cookie(REFRESH_TOKEN, refresh, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== Environment.Development,
-      sameSite: 'none',
+      sameSite: process.env.NODE_ENV !== Environment.Development ? 'none' : undefined,
     });
 
     return res.send({ access });
