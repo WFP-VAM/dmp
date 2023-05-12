ea2aed42eff6d4b613b0a7f94394939578741fee
Tasos Boulis <tboulis@hotmail.com>
2023-05-12 22:46:48 +0300

    chore(frontend): remove useGetMe and use the context auth hook (#45)

--- apps/frontend/components/pages/Home/Home.tsx
+++ apps/frontend/components/pages/Home/Home.tsx
@@ -5,23 +5,17 @@ import useSWR from 'swr';
 
 import { TableDisplay } from 'components/TableDisplay';
 import { ApiRoutes } from 'services/api/apiRoutes';
-import { useGetMe } from 'services/api/user/useUser';
 
 export const Home = (): JSX.Element => {
-  const user = useGetMe();
   const { data: lastForms, isLoading } = useSWR<DisasterDtoType[]>(
     ApiRoutes.lastForms,
   );
 
   return (
     <Box>
-      {user && (
-        <>
-          <Typography variant="h3" mb={5}>
-            <FormattedMessage id="navigation.banner" />
-          </Typography>
-        </>
-      )}
+      <Typography variant="h3" mb={5}>
+        <FormattedMessage id="navigation.banner" />
+      </Typography>
       <TableDisplay isLoading={isLoading} forms={lastForms} />
     </Box>
   );
