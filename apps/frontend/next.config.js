26133821cb3b15a85c54cb864b41576528f83b26
Eric Boucher <eric.p.boucher@gmail.com>
2023-05-12 14:16:33 +0200

    fix(devops): add standalone folder during build (#103)

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -40,6 +40,9 @@ const nextConfig = {
   swcMinify: true,
   images: { unoptimized: true },
 
+  // standalone folder for deploys
+  output: 'standalone',
+
   headers: async () => [
     {
       // Apply these headers to all routes in your application.
