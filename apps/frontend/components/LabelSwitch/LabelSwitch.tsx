3251c4a621ee326229e55e0a0c235929d86ef14f
sifelix <felixv@theodo.com>
2023-05-31 14:04:41 -0400

    feat: toggle style

--- apps/frontend/components/LabelSwitch/LabelSwitch.tsx
+++ apps/frontend/components/LabelSwitch/LabelSwitch.tsx
@@ -16,19 +16,7 @@ export const LabelSwitch = ({
     <Grid container alignItems="center" maxWidth={250}>
       <Grid item>{labelUncheck}</Grid>
       <Grid item>
-        <Switch
-          checked={value}
-          onChange={onChange}
-          sx={{
-            '& .MuiSwitch-thumb': {
-              backgroundColor: '#42A5F5',
-            },
-            '& .MuiSwitch-track': {
-              backgroundColor: '#1976d2',
-              opacity: 0.5,
-            },
-          }}
-        />
+        <Switch checked={value} onChange={onChange} />
       </Grid>
       <Grid item>{labelCheck}</Grid>
     </Grid>
