39ec572c05dfeec7d445184081217e6954c67036
Eleftherios Chaidemenos <echaidemenos@programize.com>
2024-07-11 16:56:27 +0300

    Revamp Home page

--- apps/frontend/components/Layout/Layout.tsx
+++ apps/frontend/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, useTheme } from '@mui/material';
 import { ReactNode } from 'react';
 
 interface Props {
@@ -11,6 +11,8 @@ export const Layout = ({
   children,
   backgroundColor = '#f5f5f5',
 }: Props): JSX.Element => {
+  const theme = useTheme();
+
   // Warning: the maxWidth will influence the way the PDF are generated
   // TODO investigate how to reduce this value without changing th PDF display
   return (
@@ -21,7 +23,7 @@ export const Layout = ({
       flexDirection="column"
       alignItems={alignItems ?? 'left'}
       sx={{ p: 1 }}
-      style={{ backgroundColor }}
+      style={{ backgroundColor, paddingTop: theme.spacing(5) }}
     >
       <Box maxWidth={1500}>{children}</Box>
     </Box>
--- apps/frontend/utils/date.ts
+++ apps/frontend/utils/date.ts
@@ -6,3 +6,20 @@ export const formatDate = (
 ): string => {
   return dayjs(date).format(formatStr);
 };
+
+export const getDateWeek = (currentDate: Date = new Date()) => {
+  const januaryFirst = new Date(currentDate.getFullYear(), 0, 1);
+  const daysToNextMonday =
+    januaryFirst.getDay() === 1 ? 0 : (7 - januaryFirst.getDay()) % 7;
+  const nextMonday = new Date(
+    currentDate.getFullYear(),
+    0,
+    januaryFirst.getDate() + daysToNextMonday,
+  );
+
+  return currentDate < nextMonday
+    ? 52
+    : currentDate > nextMonday
+    ? Math.ceil((+currentDate - +nextMonday) / (24 * 3600 * 1000) / 7)
+    : 1;
+};
