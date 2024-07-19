45d1773d775e04556d7c694fbb42f265fe62081c
ericboucher <eric.p.boucher@gmail.com>
2024-07-19 15:44:49 +0200

    Update DetailedReportTable.tsx

--- apps/frontend/components/DisasterTable/DetailedReportTable.tsx
+++ apps/frontend/components/DisasterTable/DetailedReportTable.tsx
@@ -13,6 +13,31 @@ interface IProps {
   border?: boolean;
 }
 
+const getLocationIds = (id: string) => {
+  return {
+    provinceId: id.slice(0, 2),
+    districtId: id.slice(0, 4),
+  };
+};
+
+type RawRow = { id: string; value: string | number | undefined; field: string };
+
+const findParentData = (
+  data: Record<string, string | number | undefined>[],
+  row: RawRow,
+) => {
+  const { provinceId, districtId } = getLocationIds(row.id);
+  console.log({ districtId });
+  const provinceData = data.find(
+    x => x.province === provinceId && x.district === undefined,
+  )?.[row.field];
+  const districtData = data.find(
+    x => x.district === districtId && x.commune === undefined,
+  )?.[row.field];
+
+  return { provinceData, districtData };
+};
+
 export const DetailedReportTable = ({
   columns,
   columnGroup,
@@ -83,17 +108,59 @@ export const DetailedReportTable = ({
     return provincesSummed.flat().flat();
   }, [data]);
 
+  // Custom comparator function
+  const customComparator = (
+    v1: number,
+    v2: number,
+    row1: RawRow,
+    row2: RawRow,
+  ) => {
+    console.log(row1);
+
+    const parentData1 = findParentData(summedData, row1);
+    const parentData2 = findParentData(summedData, row2);
+
+    if (parentData1.provinceData !== parentData2.provinceData) {
+      return (
+        (parentData2.provinceData as number) -
+        (parentData1.provinceData as number)
+      );
+    }
+    if (parentData1.districtData !== parentData2.districtData) {
+      return (
+        (parentData2.districtData as number) -
+        (parentData1.districtData as number)
+      );
+    }
+
+    return v2 - v1; // Descending order for the number column
+  };
+
+  // Add custom comparator to relevant columns
+  const columnsWithComparator = columns.map(col => {
+    if (col.type === 'number') {
+      return {
+        ...col,
+        sortComparator: customComparator,
+      };
+    }
+
+    return col;
+  });
+
+  console.log({ summedData });
+
   return (
     <DisasterTable
-      columns={addDetailedReportLocationColumns(columns)}
+      columns={addDetailedReportLocationColumns(columnsWithComparator)}
       columnGroup={columnGroup}
       data={summedData}
       isEditable={false}
       getRowId={(row: {
-        commune: string;
-        district: string;
+        commune?: string;
+        district?: string;
         province: string;
-      }) => `${row.province}${row.district}${row.commune}`}
+      }) => `${row.commune ?? row.district ?? row.province}`}
       getRowClassName={params => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
         const { district, commune } = params.row;
