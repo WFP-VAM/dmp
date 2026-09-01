b08f91e00248031911d7c3ead2763a88170ce93e
ericboucher <eric.p.boucher@gmail.com>
2026-09-01 10:34:21 +0000

    Deploying to gh-pages from @ WFP-VAM/dmp@7338c011d2df5f7f5b415a1f9a37b717dde4be8d 🚀

---
+++ _next/static/b4qkNkVthEmTkuU2P58Vj/_buildManifest.js
@@ -0,0 +1,38 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/27nlfn9-4jbw-.js"
+  ],
+  "/_error": [
+    "static/chunks/2ka9qb2s848e8.js"
+  ],
+  "/charts": [
+    "static/chunks/0o030yz75bg5n.js"
+  ],
+  "/form": [
+    "static/chunks/2yz8ap6e1q7bf.js"
+  ],
+  "/forms/search": [
+    "static/chunks/034cdde9z7aoc.js"
+  ],
+  "/login": [
+    "static/chunks/0xro8o6q1scnx.js"
+  ],
+  "/report": [
+    "static/chunks/3wt1km7gcd-xt.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/charts",
+    "/form",
+    "/forms/search",
+    "/login",
+    "/report"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
