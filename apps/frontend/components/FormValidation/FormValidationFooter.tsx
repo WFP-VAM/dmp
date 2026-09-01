7338c011d2df5f7f5b415a1f9a37b717dde4be8d
ericboucher <eric.p.boucher@gmail.com>
2026-09-01 12:33:15 +0200

    Update FormValidationFooter.tsx

--- apps/frontend/components/FormValidation/FormValidationFooter.tsx
+++ apps/frontend/components/FormValidation/FormValidationFooter.tsx
@@ -3,7 +3,7 @@ import { Cancel, Check, CheckCircle, Close, Edit } from '@mui/icons-material';
 import { CircularProgress, Stack, useTheme } from '@mui/material';
 import { DisasterType, ValidationStatusValue } from '@wfp-dmp/interfaces';
 import { useRouter } from 'next/router';
-import { FormattedMessage, useIntl } from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 import Button from 'components/Button';
 import { usePatchValidationStatus } from 'services/api/kobo/usePatchValidationStatus';
